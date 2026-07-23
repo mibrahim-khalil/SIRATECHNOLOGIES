6179dd629782c4e9450d96a2526afd10cd79273a
Muhammad Ibrahim Khalil <mibrahimkhalil564@gmail.com>
2026-07-23 21:33:08 +0500

    update cloudinary configuration

--- backend/src/config/cloudinary.js
+++ backend/src/config/cloudinary.js
@@ -1,7 +1,8 @@
 const cloudinary = require("cloudinary").v2;
 
 function connectCloudinary() {
-  const { CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET } = process.env;
+  const { CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET } =
+    process.env;
 
   if (!CLOUDINARY_CLOUD_NAME || !CLOUDINARY_API_KEY || !CLOUDINARY_API_SECRET) {
     throw new Error("Cloudinary env vars are missing");
@@ -10,7 +11,9 @@ function connectCloudinary() {
   cloudinary.config({
     cloud_name: CLOUDINARY_CLOUD_NAME,
     api_key: CLOUDINARY_API_KEY,
-    api_secret: CLOUDINARY_API_SECRET
+    api_secret: CLOUDINARY_API_SECRET,
+    timeout: 120000, // 2 minutes
+    secure: true,
   });
 
   console.log("Cloudinary configured");
