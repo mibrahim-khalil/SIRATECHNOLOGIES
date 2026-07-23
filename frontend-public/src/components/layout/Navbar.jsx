e95eb3e2f759f9d663dc8256bfa2241aae7283dc
Muhammad Ibrahim Khalil <mibrahimkhalil564@gmail.com>
2026-07-23 13:05:51 +0500

    update the nav bar

--- frontend-public/src/components/layout/Navbar.jsx
+++ frontend-public/src/components/layout/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar() {
   return (
     <header className="site-header">
       <div className="container header-row">
-        {/* Left: brand */}
+        {/* Brand */}
         <Link to="/" className="brand">
           <img
             src="/assets/logo.png"
@@ -17,42 +17,36 @@ export default function Navbar() {
           <span className="brand-text">SIRA Technologies</span>
         </Link>
 
-        {/* Center: links in your required order */}
+        {/* Professional primary nav (core pages first) */}
         <nav className="header-links">
-          {/* 1 */}
-          <NavLink to="/contact" className="header-link">
-            Help
-          </NavLink>
-
-          {/* 2 */}
-          <NavLink to="/contact" className="header-link">
-            Start a Project
-          </NavLink>
-
-          {/* 3 */}
-          <NavLink to="/pricing" className="header-link">
-            Pricing
-          </NavLink>
-
-          {/* 4 */}
           <NavLink to="/services" className="header-link">
             Services
           </NavLink>
-
-          {/* 5 */}
           <NavLink to="/portfolio" className="header-link">
             Work
           </NavLink>
-
-          {/* 6 */}
+          <NavLink to="/pricing" className="header-link">
+            Pricing
+          </NavLink>
           <NavLink to="/about" className="header-link">
             About
           </NavLink>
         </nav>
 
-        {/* Right: search + CTA */}
+        {/* Right actions: utility links + search + CTA */}
         <div className="header-actions">
+          {/* Use Link (NOT NavLink) so Help/Start don't both show active */}
+          <div className="header-utility">
+            <Link to="/contact" className="utility-link">
+              Help
+            </Link>
+            <Link to="/contact" className="utility-link">
+              Start a Project
+            </Link>
+          </div>
+
           <SearchPill placeholder="Search services" />
+
           <Button as={Link} to="/contact" variant="primary" className="quote-btn">
             Get a Quote
           </Button>
