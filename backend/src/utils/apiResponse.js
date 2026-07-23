cb6a550e6f111a6127403adbc1e389a44a97e9ae
Muhammad Ibrahim Khalil <mibrahimkhalil564@gmail.com>
2026-07-23 17:26:36 +0500

    update api response utility

---
+++ backend/src/utils/apiResponse.js
@@ -0,0 +1,22 @@
+/**
+ * Standard success response
+ */
+function success(res, data = {}, message = "Success", status = 200) {
+  return res.status(status).json({
+    success: true,
+    message,
+    data,
+  });
+}
+
+/**
+ * Standard error response
+ */
+function error(res, message = "Something went wrong", status = 500) {
+  return res.status(status).json({
+    success: false,
+    message,
+  });
+}
+
+module.exports = { success, error };
