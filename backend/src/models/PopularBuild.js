520311486dc7d0868fd34bca9006e61910040873
Muhammad Ibrahim Khalil <mibrahimkhalil564@gmail.com>
2026-07-24 18:19:06 +0500

    add popular build model

---
+++ backend/src/models/PopularBuild.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const popularBuildSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true, trim: true },
+    description: { type: String, required: true },
+    image: {
+      url: String,
+      public_id: String,
+    },
+    order: { type: Number, default: 0 },
+    isActive: { type: Boolean, default: true },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("PopularBuild", popularBuildSchema);
